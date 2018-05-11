c000853344276cfe19bde835973c2559092ec1ba
Sheerin Naseem <sheerin.naseem@gmail.com>
2018-05-11 12:01:34 +0530

    Level Scene Created - Different letters will take to different level in the game

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name)
+    {
+        Debug.Log("Level load requested for " + name);
+        #pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(name);
+        #pragma warning restore CS0618 // Type or member is obsolete
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("I wanna quit.");
+        Application.Quit();
+    }
+}
